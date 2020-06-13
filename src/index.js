cd2bd0921b152ac942bbc040423693ba3cc3ec48
foo <foo@bar.de>
2020-06-13 22:44:11 +0200

    fix: do not deploy if private

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import nuxtConfig from '@dword-design/base-config-nuxt'
 import getPackageName from 'get-package-name'
+import loadPkg from 'load-pkg'
+
+const packageConfig = loadPkg.sync()
 
 export default {
   ...nuxtConfig,
   npmPublish: false,
   useJobMatrix: false,
-  deployPlugins: [
-    getPackageName(require.resolve('@dword-design/semantic-release-vserver')),
-  ],
-  deployEnv: {
-    SSH_USER: '${{ secrets.SSH_USER }}',
-    SSH_PRIVATE_KEY: '${{ secrets.SSH_PRIVATE_KEY }}',
-  },
+  ...(!packageConfig.private && {
+    deployPlugins: [
+      getPackageName(require.resolve('@dword-design/semantic-release-vserver')),
+    ],
+    deployEnv: {
+      SSH_USER: '${{ secrets.SSH_USER }}',
+      SSH_PRIVATE_KEY: '${{ secrets.SSH_PRIVATE_KEY }}',
+    },
+  }),
 }
