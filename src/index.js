0aed2cf5c7b5cfd25cbb32fd463e2091498a79de
foo <foo@bar.de>
2020-07-17 11:12:55 +0200

    feat: use pm2 for deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import nuxtConfig from '@dword-design/base-config-nuxt'
 import execa from 'execa'
+import { copy } from 'fs-extra'
 import getPackageName from 'get-package-name'
 import loadPkg from 'load-pkg'
 
@@ -7,7 +8,15 @@ const packageConfig = loadPkg.sync()
 
 export default {
   ...nuxtConfig,
-  allowedMatches: [...nuxtConfig.allowedMatches, '.ceilingrc.json'],
+  prepare: async () => {
+    await nuxtConfig.prepare()
+    return copy(require.resolve('ecosystem.json'), '.')
+  },
+  allowedMatches: [
+    ...nuxtConfig.allowedMatches,
+    '.ceilingrc.json',
+    'ecosystem.json',
+  ],
   gitignore: [...nuxtConfig.gitignore, '/.ceilingrc.json'],
   packageConfig: {
     main: 'dist/index.js',
@@ -16,7 +25,14 @@ export default {
   useJobMatrix: false,
   ...(!packageConfig.private && {
     deployPlugins: [
-      getPackageName(require.resolve('@dword-design/semantic-release-vserver')),
+      [
+        getPackageName(require.resolve('@semantic-release/exec')),
+        {
+          publishCmd: `${getPackageName(
+            require.resolve('pm2')
+          )} deploy production`,
+        },
+      ],
     ],
     deployEnv: {
       SSH_USER: '${{ secrets.SSH_USER }}',
