87fbb77e8d1def3e642049f0c22684bedf3f4469
Sebastian Landwehr <info@dword-design.de>
2021-02-20 15:08:53 +0100

    fix: always use ssh url for deployment

--- src/ecosystem.js
+++ src/ecosystem.js
@@ -1,4 +1,5 @@
 import { existsSync } from 'fs-extra'
+import hostedGitInfo from 'hosted-git-info'
 import loadPkg from 'load-pkg'
 import parseGitConfig from 'parse-git-config'
 import parsePkgName from 'parse-pkg-name'
@@ -7,6 +8,10 @@ import P from 'path'
 const repositoryUrl = existsSync('.git')
   ? parseGitConfig.sync()['remote "origin"']?.url
   : undefined
+const gitInfo = hostedGitInfo.fromUrl(repositoryUrl) || {}
+if (repositoryUrl !== undefined && gitInfo.type !== 'github') {
+  throw new Error('Only GitHub repositories are supported.')
+}
 const packageConfig = loadPkg.sync()
 const packageName = parsePkgName(packageConfig.name).name
 
@@ -25,7 +30,9 @@ export default {
       host: ['dword-design.de'],
       path: `/var/www/${packageName}`,
       user: 'root',
-      ...(repositoryUrl && { repo: repositoryUrl }),
+      ...(repositoryUrl && {
+        repo: `git@github.com:${gitInfo.user}/${gitInfo.project}.git`,
+      }),
       'post-deploy': `source ~/.nvm/nvm.sh && yarn --frozen-lockfile && node --max-old-space-size=512 ./node_modules/.bin/nuxt-babel build --config-file ${P.relative(
         process.cwd(),
         require.resolve('@dword-design/base-config-nuxt/dist/nuxt.config.js')
---
+++ src/ecosystem.spec.js
@@ -0,0 +1,31 @@
+import proxyquire from '@dword-design/proxyquire'
+import execa from 'execa'
+import { outputFile } from 'fs-extra'
+import withLocalTmpDir from 'with-local-tmp-dir'
+
+export default {
+  'git https url': () =>
+    withLocalTmpDir(async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'foo' }))
+      await execa.command('git init')
+      await execa.command(
+        'git remote add origin https://github.com/dword-design/foo.git'
+      )
+      const self = proxyquire('./ecosystem', {})
+      expect(self.deploy.production.repo).toEqual(
+        'git@github.com:dword-design/foo.git'
+      )
+    }),
+  'git ssh url': () =>
+    withLocalTmpDir(async () => {
+      await outputFile('package.json', JSON.stringify({ name: 'foo' }))
+      await execa.command('git init')
+      await execa.command(
+        'git remote add origin git@github.com:dword-design/foo.git'
+      )
+      const self = proxyquire('./ecosystem', {})
+      expect(self.deploy.production.repo).toEqual(
+        'git@github.com:dword-design/foo.git'
+      )
+    }),
+}
