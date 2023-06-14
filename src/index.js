1bf68d49f35f7bfacad9acf37e80c9676831d86b
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-06-14 23:42:16 +0000

    fix: extends

--- src/index.js
+++ src/index.js
@@ -7,78 +7,88 @@ import yaml from 'yaml'
 
 import dockerCompose from './docker-compose.js'
 import getEcosystemConfig from './get-ecosystem-config.js'
-import nginxConfig from './nginx-config.js'
+import getNginxConfig from './get-nginx-config.js'
 
-const packageConfig = loadPkg.sync()
+export default () => {
+  const packageConfig = loadPkg.sync()
 
-export default {
-  ...baseConfigNuxt,
-  allowedMatches: [
-    ...baseConfigNuxt.allowedMatches,
-    'docker-compose.yml',
-    'ecosystem.json',
-    'nginx',
-  ],
-  editorIgnore: [
-    ...baseConfigNuxt.editorIgnore,
-    'docker-compose.yml',
-    'ecosystem.json',
-    'nginx',
-  ],
-  gitignore: [
-    ...baseConfigNuxt.gitignore,
-    '/.ceilingrc.json',
-    '/nginx/default.config',
-  ],
-  isLockFileFixCommitType: true,
-  npmPublish: false,
-  packageConfig: {
-    main: 'dist/index.js',
-  },
-  prepare: async () => {
-    await baseConfigNuxt.prepare()
+  return {
+    ...baseConfigNuxt,
+    allowedMatches: [
+      ...baseConfigNuxt.allowedMatches,
+      'docker-compose.yml',
+      'ecosystem.json',
+      'nginx',
+    ],
+    editorIgnore: [
+      ...baseConfigNuxt.editorIgnore,
+      'docker-compose.yml',
+      'ecosystem.json',
+      'nginx',
+    ],
+    gitignore: [
+      ...baseConfigNuxt.gitignore,
+      '/.ceilingrc.json',
+      '/nginx/default.config',
+    ],
+    isLockFileFixCommitType: true,
+    npmPublish: false,
+    packageConfig: {
+      main: 'dist/index.js',
+    },
+    prepare: async () => {
+      await baseConfigNuxt.prepare()
 
-    return outputFiles({
-      'docker-compose.yml': yaml.stringify(dockerCompose),
-      'ecosystem.json': JSON.stringify(getEcosystemConfig(), undefined, 2),
-      'nginx/default.config': JSON.stringify(nginxConfig, undefined, 2),
-    })
-  },
-  useJobMatrix: false,
-  ...(!packageConfig.private && {
-    deployPlugins: [
-      [
-        packageName`@semantic-release/exec`,
-        {
-          publishCmd: `${packageName`pm2`} deploy production --force`,
-        },
+      return outputFiles({
+        'docker-compose.yml': yaml.stringify(dockerCompose),
+        'ecosystem.json': JSON.stringify(
+          getEcosystemConfig(packageConfig),
+          undefined,
+          2,
+        ),
+        'nginx/default.config': JSON.stringify(
+          getNginxConfig(packageConfig),
+          undefined,
+          2,
+        ),
+      })
+    },
+    useJobMatrix: false,
+    ...(!packageConfig.private && {
+      deployPlugins: [
+        [
+          packageName`@semantic-release/exec`,
+          {
+            publishCmd: `${packageName`pm2`} deploy production --force`,
+          },
+        ],
       ],
-    ],
-    preDeploySteps: [
-      {
-        uses: 'webfactory/ssh-agent@v0.5.1',
-        with: {
-          'ssh-private-key': '${{ secrets.SSH_PRIVATE_KEY }}',
+      preDeploySteps: [
+        {
+          uses: 'webfactory/ssh-agent@v0.5.1',
+          with: {
+            'ssh-private-key': '${{ secrets.SSH_PRIVATE_KEY }}',
+          },
         },
+        { run: 'ssh-keyscan sebastianlandwehr.com >> ~/.ssh/known_hosts' },
+      ],
+    }),
+    commands: {
+      ...baseConfigNuxt.commands,
+      pull: {
+        arguments: '<endpoint>',
+        handler: endpoint =>
+          execaCommand(`ceiling pull ${endpoint}`, { stdio: 'inherit' }),
       },
-      { run: 'ssh-keyscan sebastianlandwehr.com >> ~/.ssh/known_hosts' },
-    ],
-  }),
-  commands: {
-    ...baseConfigNuxt.commands,
-    pull: {
-      arguments: '<endpoint>',
-      handler: endpoint =>
-        execaCommand(`ceiling pull ${endpoint}`, { stdio: 'inherit' }),
+      push: {
+        arguments: '<endpoint>',
+        handler: endpoint =>
+          execaCommand(`ceiling push ${endpoint}`, { stdio: 'inherit' }),
+      },
+      setupDeploy: {
+        handler: () =>
+          execaCommand('pm2 deploy production setup', { stdio: 'inherit' }),
+      },
     },
-    push: {
-      arguments: '<endpoint>',
-      handler: endpoint =>
-        execaCommand(`ceiling push ${endpoint}`, { stdio: 'inherit' }),
-    },
-    setupDeploy: {
-      handler: () =>
-        execaCommand('pm2 deploy production setup', { stdio: 'inherit' }),
-    },
-  },
+  }
 }
