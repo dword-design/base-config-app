745f9512fb39076a2083cb5cb5646a675c249155
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-27 19:36:42 +0200

    fix: foo

--- src/get-ecosystem-config.spec.js
+++ src/get-ecosystem-config.spec.js
@@ -1,33 +1,29 @@
-import tester from '@dword-design/tester';
-import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir';
+import { expect } from '@playwright/test';
 import { execaCommand } from 'execa';
+import { test } from 'playwright-local-tmp-dir';
 
 import self from './get-ecosystem-config.js';
 
-export default tester(
-  {
-    'git https url': async () => {
-      await execaCommand('git init');
+test('git https url', async () => {
+  await execaCommand('git init');
 
-      await execaCommand(
-        'git remote add origin https://github.com/dword-design/foo.git',
-      );
+  await execaCommand(
+    'git remote add origin https://github.com/dword-design/foo.git',
+  );
 
-      expect(self({ name: 'foo' }).deploy.production.repo).toEqual(
-        'git@github.com:dword-design/foo.git',
-      );
-    },
-    'git ssh url': async () => {
-      await execaCommand('git init');
+  expect(self({ name: 'foo' }).deploy.production.repo).toEqual(
+    'git@github.com:dword-design/foo.git',
+  );
+});
 
-      await execaCommand(
-        'git remote add origin git@github.com:dword-design/foo.git',
-      );
+test('git ssh url', async () => {
+  await execaCommand('git init');
 
-      expect(self({ name: 'foo' }).deploy.production.repo).toEqual(
-        'git@github.com:dword-design/foo.git',
-      );
-    },
-  },
-  [testerPluginTmpDir()],
-);
+  await execaCommand(
+    'git remote add origin git@github.com:dword-design/foo.git',
+  );
+
+  expect(self({ name: 'foo' }).deploy.production.repo).toEqual(
+    'git@github.com:dword-design/foo.git',
+  );
+});
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,42 +1,32 @@
 import { Base } from '@dword-design/base';
 import { endent } from '@dword-design/functions';
-import tester from '@dword-design/tester';
-import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer';
-import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir';
+import { expect } from '@playwright/test';
 import nuxtDevReady from 'nuxt-dev-ready';
 import outputFiles from 'output-files';
+import { test } from 'playwright-local-tmp-dir';
 import kill from 'tree-kill-promise';
 
-export default tester(
-  {
-    async dev() {
-      await outputFiles({
-        'config.js': endent`
-          export default {
-            name: 'Foo',
-          }
-        `,
-        'package.json': JSON.stringify({}),
-        'pages/index.vue': endent`
-          <template>
-            <div class="foo" />
-          </template>
-        `,
-      });
+test('dev', async ({ page }) => {
+  await outputFiles({
+    'config.js': "export default { name: 'Foo' }",
+    'package.json': JSON.stringify({}),
+    'pages/index.vue': endent`
+      <template>
+        <div class="foo" />
+      </template>
+    `,
+  });
 
-      const base = new Base({ name: '../src/index.js' });
-      await base.prepare();
-      const nuxt = base.run('dev');
+  const base = new Base({ name: '../src/index.js' });
+  await base.prepare();
+  const nuxt = base.run('dev');
 
-      try {
-        await nuxtDevReady();
-        await this.page.goto('http://localhost:3000');
-        await this.page.$('.foo');
-        expect(await this.page.title()).toEqual('Foo');
-      } finally {
-        await kill(nuxt.pid);
-      }
-    },
-  },
-  [testerPluginPuppeteer(), testerPluginTmpDir()],
-);
+  try {
+    await nuxtDevReady();
+    await page.goto('http://localhost:3000');
+    await expect(page.locator('.foo')).toBeAttached();
+    expect(await page.title()).toEqual('Foo');
+  } finally {
+    await kill(nuxt.pid);
+  }
+});
