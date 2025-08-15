99b5bdeb1cd45e18505cfc93cc3a1ca762947b56
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-08-15 17:11:13 +0200

    fix: foo

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ export default function (config) {
       deployPlugins: [
         [
           packageName`@semantic-release/exec`,
-          { publishCmd: `${packageName`pm2`} deploy production` },
+          { publishCmd: `git rev-list @{upstream}.. -n 1 && ${packageName`pm2`} deploy production` },
         ],
       ],
       preDeploySteps: [
